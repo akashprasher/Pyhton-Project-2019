9c1653a8fae0a1e03563e0eb2137011f15d84538
Akash Prasher <42891970+akashprasher@users.noreply.github.com>
2019-03-02 16:04:17 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head align="right">
+<title>heart.a.traveller-login</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="ti.png" type="image/png">
+<style>
+
+body {  background-repeat: no-repeat; background-attachment: scroll;
+   
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+
+.topnav {
+  overflow: hidden;
+  background-color: #ffffff;
+}
+
+.topnav a {
+  float: right;
+  color: black;
+  text-align: center;
+  padding: 10px 10px;
+  text-decoration: none;
+  font-size: 17px;
+  
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: white;
+}
+
+
+.dopnav {
+  overflow: hidden;
+  background-color: #ffffff;
+  
+}
+.dopnav a {
+  float: center;
+  color: black;
+  text-align: center;
+  padding: 10px 10px;
+  text-decoration: none;
+  font-size: 17px;
+  
+}
+.dopnav a:hover {
+  background-color: #ddd;
+  color: white;
+}
+.center {
+    align-self: center;
+    margin: auto;
+    width: 60%;
+    border: 3px solid green;
+    padding: 10px;
+    background-color: white;
+    opacity: 0.8;
+}
+
+</style>
+</head>
+<body style="align-items: center;" style="size: 100px" background="imgl.jpg" >
+<div class="topnav" align="center">
+  <p><img src="ehdr.png" style="width:189px;height:57.2px;" style="background-color: green " ></p>
+</div>
+<div class="topnav" align="right" style="background-color: green" opacity: 0.4;>
+   <a href="about.html">About</a>
+   <a href="contact.html">Contact</a>
+   <a href="#login">Login</a>
+   <a href="#Example">Gallery</a>
+   <a href="home.html">Home</a>
+
+
+</div>
+<br><br><br><br><br><br>
+<form action="login.php" align="center">
+<div class="center">
+<img src="elogin.png" alt="contact" width="189px" height="57.2px"></img>
+<h4 style="color: green">Email</h4>
+<input type="Email" placeholder="Enter your email" name="email">
+<h4 style="color: green">Password</h4>
+<input  type="Password" placeholder="Password" name="password">
+<br><br>
+<button style="color: green">Login</button><br>
+<input type="submit" name="submit">
+<input type="text" name="txt">
+<h6>Don't Have an Account, Please Signup</h6><button style="color: green">Signup</button>
+</div>
+</form>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="parallax"></div>
+<div class="dopnav" align="center" style="background-color: green" opacity: 0.4;>
+   <a href="home.html">Home</a>
+   <a href="#Example">Example</a>
+   <a href="contact.html">Contact</a>
+   <a href="abouthtml">About</a>
+   
+   
+   
+   
+
+
+</div>
+
+
+
+</body>
+</html>
